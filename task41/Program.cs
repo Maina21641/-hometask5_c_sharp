117faf93ca19ee7e78dd4640560610a5fd4d5e05
Alex <Yulien.ru@Yandex.ru>
2022-09-08 19:44:32 +0400

    added number user > 0

---
+++ task41/Program.cs
@@ -0,0 +1,11 @@
+﻿void Task41() {
+    string input = Console.ReadLine();
+    int count = 0;
+    while (input.ToLower() != "stop") {
+        if (int.Parse(input) > 0) count++;
+        input = Console.ReadLine();
+    }
+    Console.WriteLine(count);
+}
+
+Task41();
